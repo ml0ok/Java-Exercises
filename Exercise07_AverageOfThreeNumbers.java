3ddd4aa3b7c1ea46ed4b02a63113a57725e6aec3
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:22:42 +0300

    Update Exercise07_AverageOfThreeNumbers.java

--- Exercise07_AverageOfThreeNumbers.java
+++ Exercise07_AverageOfThreeNumbers.java
@@ -1,1 +1,23 @@
-import java.util.*; public class Exercise07_AverageOfThreeNumbers{public static void main(String[]a){Scanner s=new Scanner(System.in);int x=s.nextInt(),y=s.nextInt(),z=s.nextInt();System.out.println((x+y+z)/3.0);}}
+import java.util.Scanner;
+
+public class Exercise07_AverageOfThreeNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input first number: ");
+        int x = s.nextInt();
+
+        System.out.print("Input second number: ");
+        int y = s.nextInt();
+
+        System.out.print("Input third number: ");
+        int z = s.nextInt();
+
+        double average = (x + y + z) / 3.0;
+
+        System.out.println("Average = " + average);
+
+    }
+}
