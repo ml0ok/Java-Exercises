14e6d78f17bf6612b7cd08be85a7e267c42fcd9a
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:21:06 +0300

    Update Exercise02_DivisionOfTwoNumbers.java

--- Exercise02_DivisionOfTwoNumbers.java
+++ Exercise02_DivisionOfTwoNumbers.java
@@ -1,1 +1,8 @@
-public class Exercise02_DivisionOfTwoNumbers{public static void main(String[]a){System.out.println(60/3);}}
+public class Exercise02_DivisionOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        System.out.println(60 / 3);
+
+    }
+}
