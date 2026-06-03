4aed8864304620771c69f43dd2e7e8af6fe76653
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:21:43 +0300

    Update Exercise04_ProductOfTwoNumbers.java

--- Exercise04_ProductOfTwoNumbers.java
+++ Exercise04_ProductOfTwoNumbers.java
@@ -1,1 +1,18 @@
-import java.util.*; public class Exercise04_ProductOfTwoNumbers{public static void main(String[]a){Scanner s=new Scanner(System.in);int x=s.nextInt(),y=s.nextInt();System.out.println(x*y);}}
+import java.util.Scanner;
+
+public class Exercise04_ProductOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input first number: ");
+        int x = s.nextInt();
+
+        System.out.print("Input second number: ");
+        int y = s.nextInt();
+
+        System.out.println(x + " x " + y + " = " + (x * y));
+
+    }
+}
