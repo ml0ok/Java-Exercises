f51e38fa44b6162cfb470800ce2ed76eb516e897
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:23:48 +0300

    Update Exercise10_BreakIntegerIntoDigits.java

--- Exercise10_BreakIntegerIntoDigits.java
+++ Exercise10_BreakIntegerIntoDigits.java
@@ -1,1 +1,17 @@
-import java.util.*; public class Exercise10_BreakIntegerIntoDigits{public static void main(String[]a){Scanner s=new Scanner(System.in);String n=s.next();for(char c:n.toCharArray())System.out.print(c+" ");}}
+import java.util.Scanner;
+
+public class Exercise10_BreakIntegerIntoDigits {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input six non-negative digits: ");
+        String number = s.next();
+
+        for (char digit : number.toCharArray()) {
+            System.out.print(digit + " ");
+        }
+
+    }
+}
