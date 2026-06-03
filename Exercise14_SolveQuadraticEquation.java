9fabb41808c9b07f1f9b647b2305ebe2a0641d49
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:25:57 +0300

    Update Exercise14_SolveQuadraticEquation.java

--- Exercise14_SolveQuadraticEquation.java
+++ Exercise14_SolveQuadraticEquation.java
@@ -1,1 +1,39 @@
-import java.util.*; public class Exercise14_SolveQuadraticEquation{public static void main(String[]a){Scanner s=new Scanner(System.in);double A=s.nextDouble(),B=s.nextDouble(),C=s.nextDouble();double d=B*B-4*A*C;if(d>=0){System.out.println(( -B+Math.sqrt(d))/(2*A));System.out.println(( -B-Math.sqrt(d))/(2*A));}}}
+import java.util.Scanner;
+
+public class Exercise14_SolveQuadraticEquation {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input a: ");
+        double a = s.nextDouble();
+
+        System.out.print("Input b: ");
+        double b = s.nextDouble();
+
+        System.out.print("Input c: ");
+        double c = s.nextDouble();
+
+        double d = b * b - 4 * a * c;
+
+        if (d > 0) {
+
+            double r1 = (-b + Math.sqrt(d)) / (2 * a);
+            double r2 = (-b - Math.sqrt(d)) / (2 * a);
+
+            System.out.println("The roots are " + r1 + " and " + r2);
+
+        } else if (d == 0) {
+
+            double r = -b / (2 * a);
+
+            System.out.println("The root is " + r);
+
+        } else {
+
+            System.out.println("The equation has no real roots");
+
+        }
+    }
+}
