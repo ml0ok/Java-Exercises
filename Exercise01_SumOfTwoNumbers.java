4a1d751e8ac82178cf6bf63a1c982111bd35ff49
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:20:45 +0300

    Format Exercise01_SumOfTwoNumbers.java for readability

--- Exercise01_SumOfTwoNumbers.java
+++ Exercise01_SumOfTwoNumbers.java
@@ -1,1 +1,8 @@
-public class Exercise01_SumOfTwoNumbers{public static void main(String[]a){System.out.println(70+36);}}
+public class Exercise01_SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        System.out.println(70 + 36);
+
+    }
+}
