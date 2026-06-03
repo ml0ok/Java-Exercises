eee457c683f2f77fcbd8c3edd0dd02d2031f9643
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:27:17 +0300

    Update Exercise18_DaysInAMonth.java

--- Exercise18_DaysInAMonth.java
+++ Exercise18_DaysInAMonth.java
@@ -1,1 +1,21 @@
-import java.time.*;import java.util.*; public class Exercise18_DaysInAMonth{public static void main(String[]a){Scanner s=new Scanner(System.in);int m=s.nextInt(),y=s.nextInt();System.out.println(YearMonth.of(y,m).lengthOfMonth());}}
+import java.time.YearMonth;
+import java.util.Scanner;
+
+public class Exercise18_DaysInAMonth {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input a month number: ");
+        int month = s.nextInt();
+
+        System.out.print("Input a year: ");
+        int year = s.nextInt();
+
+        int days = YearMonth.of(year, month).lengthOfMonth();
+
+        System.out.println(days + " days");
+
+    }
+}
