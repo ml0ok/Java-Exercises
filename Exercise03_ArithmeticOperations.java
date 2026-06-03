0aec8074150cd16b5add2bf323bb810fe6d4f552
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:21:25 +0300

    Update Exercise03_ArithmeticOperations.java

--- Exercise03_ArithmeticOperations.java
+++ Exercise03_ArithmeticOperations.java
@@ -1,1 +1,11 @@
-public class Exercise03_ArithmeticOperations{public static void main(String[]a){System.out.println(-5+8*6);System.out.println((55+9)%9);System.out.println(20+-3*5/8);System.out.println(5+15/3*2-8%3);}}
+public class Exercise03_ArithmeticOperations {
+
+    public static void main(String[] args) {
+
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+    }
+}
