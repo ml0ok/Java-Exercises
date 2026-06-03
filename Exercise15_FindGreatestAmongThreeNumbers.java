970f53583b1502a4bd33d280040acbfde1f0daf5
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:26:17 +0300

    Update Exercise15_FindGreatestAmongThreeNumbers.java

--- Exercise15_FindGreatestAmongThreeNumbers.java
+++ Exercise15_FindGreatestAmongThreeNumbers.java
@@ -1,1 +1,29 @@
-import java.util.*; public class Exercise15_FindGreatestAmongThreeNumbers{public static void main(String[]a){Scanner s=new Scanner(System.in);int x=s.nextInt(),y=s.nextInt(),z=s.nextInt();int m=x;if(y>m)m=y;if(z>m)m=z;System.out.println(m);}}
+import java.util.Scanner;
+
+public class Exercise15_FindGreatestAmongThreeNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input the 1st number: ");
+        int x = s.nextInt();
+
+        System.out.print("Input the 2nd number: ");
+        int y = s.nextInt();
+
+        System.out.print("Input the 3rd number: ");
+        int z = s.nextInt();
+
+        int max = x;
+
+        if (y > max)
+            max = y;
+
+        if (z > max)
+            max = z;
+
+        System.out.println("The greatest: " + max);
+
+    }
+}
