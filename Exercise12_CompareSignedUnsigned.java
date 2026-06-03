266f271599446a2224f14e6d5420ef6e95092f35
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:25:13 +0300

    Update Exercise12_CompareSignedUnsigned.java

--- Exercise12_CompareSignedUnsigned.java
+++ Exercise12_CompareSignedUnsigned.java
@@ -1,1 +1,12 @@
-public class Exercise12_CompareSignedUnsigned{public static void main(String[]a){int x=-1,y=1;System.out.println(Integer.compareUnsigned(x,y));}}
+public class Exercise12_CompareSignedUnsigned {
+
+    public static void main(String[] args) {
+
+        int x = -1;
+        int y = 1;
+
+        System.out.println("Signed comparison: " + Integer.compare(x, y));
+        System.out.println("Unsigned comparison: " + Integer.compareUnsigned(x, y));
+
+    }
+}
