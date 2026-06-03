8a511cd6fd73a2a48a4140d39cad1cae94ff273f
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:22:04 +0300

    Update Exercise05_BasicArithmeticOperations.java

--- Exercise05_BasicArithmeticOperations.java
+++ Exercise05_BasicArithmeticOperations.java
@@ -1,1 +1,22 @@
-import java.util.*; public class Exercise05_BasicArithmeticOperations{public static void main(String[]a){Scanner s=new Scanner(System.in);int x=s.nextInt(),y=s.nextInt();System.out.println(x+y);System.out.println(x-y);System.out.println(x*y);System.out.println(x/y);System.out.println(x%y);}}
+import java.util.Scanner;
+
+public class Exercise05_BasicArithmeticOperations {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input first number: ");
+        int x = s.nextInt();
+
+        System.out.print("Input second number: ");
+        int y = s.nextInt();
+
+        System.out.println(x + " + " + y + " = " + (x + y));
+        System.out.println(x + " - " + y + " = " + (x - y));
+        System.out.println(x + " x " + y + " = " + (x * y));
+        System.out.println(x + " / " + y + " = " + (x / y));
+        System.out.println(x + " mod " + y + " = " + (x % y));
+
+    }
+}
