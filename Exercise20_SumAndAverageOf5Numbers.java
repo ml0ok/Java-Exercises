022b78d83383acb170a46e31dfcf7df7e0321afc
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:28:02 +0300

    Update Exercise20_SumAndAverageOf5Numbers.java

--- Exercise20_SumAndAverageOf5Numbers.java
+++ Exercise20_SumAndAverageOf5Numbers.java
@@ -1,1 +1,23 @@
-import java.util.*; public class Exercise20_SumAndAverageOf5Numbers{public static void main(String[]a){Scanner s=new Scanner(System.in);int sum=0;for(int i=0;i<5;i++)sum+=s.nextInt();System.out.println("Sum="+sum);System.out.println("Average="+(sum/5.0));}}
+import java.util.Scanner;
+
+public class Exercise20_SumAndAverageOf5Numbers {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int sum = 0;
+
+        System.out.print("Input the 5 numbers: ");
+
+        for (int i = 0; i < 5; i++) {
+            sum += s.nextInt();
+        }
+
+        double average = sum / 5.0;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+    }
+}
