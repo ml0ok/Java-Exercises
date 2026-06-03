fc2ce0a7d9986721b0e5be391244593a9ba5200d
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:24:12 +0300

    Update Exercise11_CheckFiniteFloatingPoint.java

--- Exercise11_CheckFiniteFloatingPoint.java
+++ Exercise11_CheckFiniteFloatingPoint.java
@@ -1,1 +1,19 @@
-public class Exercise11_CheckFiniteFloatingPoint{public static void main(String[]a){double x=10.5;System.out.println(Double.isFinite(x));}}
+import java.util.Scanner;
+
+public class Exercise11_CheckFiniteFloatingPoint {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input a double value: ");
+        double x = s.nextDouble();
+
+        if (Double.isFinite(x)) {
+            System.out.println("Finite floating-point value");
+        } else {
+            System.out.println("Not a finite floating-point value");
+        }
+
+    }
+}
