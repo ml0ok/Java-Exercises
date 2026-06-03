7b33bc8f44fc3d31df168ec485cf2cf992660f3d
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:23:29 +0300

    Update Exercise09_ArithmeticOperationsOnTwoIntegers.java

--- Exercise09_ArithmeticOperationsOnTwoIntegers.java
+++ Exercise09_ArithmeticOperationsOnTwoIntegers.java
@@ -1,1 +1,24 @@
-import java.util.*; public class Exercise09_ArithmeticOperationsOnTwoIntegers{public static void main(String[]a){Scanner s=new Scanner(System.in);int x=s.nextInt(),y=s.nextInt();System.out.println("Sum="+(x+y));}}
+import java.util.Scanner;
+
+public class Exercise09_ArithmeticOperationsOnTwoIntegers {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input 1st integer: ");
+        int x = s.nextInt();
+
+        System.out.print("Input 2nd integer: ");
+        int y = s.nextInt();
+
+        System.out.println("Sum of two integers: " + (x + y));
+        System.out.println("Difference of two integers: " + (x - y));
+        System.out.println("Product of two integers: " + (x * y));
+        System.out.println("Average of two integers: " + ((x + y) / 2.0));
+        System.out.println("Distance of two integers: " + Math.abs(x - y));
+        System.out.println("Max integer: " + Math.max(x, y));
+        System.out.println("Min integer: " + Math.min(x, y));
+
+    }
+}
