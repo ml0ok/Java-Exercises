6b786dccc1294f4a1cdfaebb89b2c4dafa2e21bc
ml0ok <mlook19.k@gmail.com>
2026-06-04 01:26:35 +0300

    Update Exercise16_CheckPositiveNegativeOrZero.java

--- Exercise16_CheckPositiveNegativeOrZero.java
+++ Exercise16_CheckPositiveNegativeOrZero.java
@@ -1,1 +1,29 @@
-import java.util.*; public class Exercise16_CheckPositiveNegativeOrZero{public static void main(String[]a){Scanner s=new Scanner(System.in);double n=s.nextDouble();if(n==0)System.out.println("zero");else if(n>0)System.out.println("positive");else System.out.println("negative");}}
+import java.util.Scanner;
+
+public class Exercise16_CheckPositiveNegativeOrZero {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        double n = s.nextDouble();
+
+        if (n == 0) {
+
+            System.out.println("zero");
+
+        } else {
+
+            if (n > 0)
+                System.out.print("positive ");
+            else
+                System.out.print("negative ");
+
+            if (Math.abs(n) < 1)
+                System.out.print("small");
+            else if (Math.abs(n) > 1000000)
+                System.out.print("large");
+        }
+    }
+}
