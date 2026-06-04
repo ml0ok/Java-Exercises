1bbd6be745a028b832c39581abb3fc8aba746ffe
ml0ok <mlook19.k@gmail.com>
2026-06-04 05:21:33 +0300

    Update Q5_GreatestNumber.java

--- Q5_GreatestNumber.java
+++ Q5_GreatestNumber.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
-public class Q5_GreatestNumber {
+
+public class Q5 {
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
-        int a=s.nextInt(), b=s.nextInt(), c=s.nextInt();
-        int max=a; if(b>max) max=b; if(c>max) max=c;
+
+        System.out.print("Input the 1st number: ");
+        int a = s.nextInt();
+
+        System.out.print("Input the 2nd number: ");
+        int b = s.nextInt();
+
+        System.out.print("Input the 3rd number: ");
+        int c = s.nextInt();
+
+        int max = a;
+
+        if (b > max)
+            max = b;
+
+        if (c > max)
+            max = c;
+
         System.out.println("The greatest: " + max);
     }
 }
